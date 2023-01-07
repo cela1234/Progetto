629fb7a1208e50d67714f591ebcf458acd3ed04b
cela1234 <alessiocela02@gmail.com>
2023-01-07 15:52:44 +0100

    b

--- src/progetto/Prodotto.java
+++ src/progetto/Prodotto.java
@@ -14,4 +14,29 @@ public class Prodotto {
         this.tempoDiProduzione = tempoDiProduzione;
         this.catenaDiMacchinari = catenaDiMacchinari;
     }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public int getTempoDiProduzione() {
+        return tempoDiProduzione;
+    }
+    public void setTempoDiProduzione(int tempoDiProduzione) {
+        this.tempoDiProduzione = tempoDiProduzione;
+    }
+    public ArrayList<String> getCatenaDiMacchinari() {
+        return catenaDiMacchinari;
+    }
+    public void setCatenaDiMacchinari(ArrayList<String> catenaDiMacchinari) {
+        this.catenaDiMacchinari = catenaDiMacchinari;
+    }
 }

efd0ff4d0c545617141d43c714e61d93671a51d6
cela1234 <alessiocela02@gmail.com>
2023-01-07 15:32:55 +0100

    b

--- src/progetto/Progetto.java
+++ src/progetto/Progetto.java
@@ -70,7 +70,37 @@ public class Progetto {
             for(int j = 0; j < macchinariInConflitto.length; j++) { conflitti.add(macchinariInConflitto[j]);}
             macchinari[i] = new Macchinario(ArrayDaAnalizzare[0], ArrayDaAnalizzare[1], Integer.parseInt(numeriRange[0]), Integer.parseInt(numeriRange[1]), ArrayDaAnalizzare[3], conflitti);
         }
-        for(int i = 0; i < prodotti.length; i++)
-
+        for(int i = 0; i < prodotti.length; i++){
+            toAnalyze = input.nextLine();
+            String[] ArrayDaAnalizzare = toAnalyze.split(" ");
+            String[] catenaArray = ArrayDaAnalizzare[3].split("->");
+            ArrayList<String> catena = new ArrayList<>();
+            for(int j = 0; j < catenaArray.length; j++) {catena.add(catenaArray[j]);}
+        }
+        for(int i = 0; i < lavoratori.length; i++){
+            toAnalyze = input.nextLine();
+            String[] ArrayToAnalyze = toAnalyze.split(" -> ");
+            String[] macchinariDaAssegnareAlLavoratore = ArrayToAnalyze[1].split(",");
+            for(int j = 0; j < lavoratori.length; j++){
+                if (lavoratori[j].getIdLavoratore().equals(ArrayToAnalyze[0]))
+                    for (int k = 0; k < macchinariDaAssegnareAlLavoratore.length; k++){
+                        lavoratori[j].assegnaMacchinarioLavoratore(macchinariDaAssegnareAlLavoratore[k]);
+                    }
+            }
+        }
+        toAnalyze = input.nextLine();
+        String[] resolveTask = toAnalyze.split(" ");
+        if (resolveTask[0].equalsIgnoreCase("TASK1"))
+        {
+            //task1
+        }
+        else if (resolveTask[0].equalsIgnoreCase("TASK2")) //formato task2 nell'input: "TASK2 "
+        {
+            //task2
+        }
+        else if (resolveTask[0].equalsIgnoreCase("TASK3"))
+        {
+            //task3
+        }
     }
 }
